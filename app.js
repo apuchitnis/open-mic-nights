d4335f0040d21244a30e0f0913c62df77cb51b1f
Apurva Chitnis <apuchitnis@hotmail.com>
2019-09-08 19:08:33 +0100

    Initial commit.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./bulma.min.css";
+import GoogleSpreadsheet from "google-spreadsheet";
 
 class Results extends React.Component {
   constructor(props) {
@@ -47,27 +46,60 @@ class Results extends React.Component {
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: ["Afghanistan",
-      "Åland Islands",
-      "Albania",
-      "Algeria"]
-    };
-  }
+    constructor(props) {
+	super(props);	
+	this.state = {
+	    isFetching: true,
+	    results: ["Afghanistan",
+		      "Åland Islands",
+		      "Albania",
+		      "Algeria"]
+	};
+    }
 
-  render() {
-    return (
-      <div className="content">
-        <div className="container">
-          <section className="section">
-              <Results results={this.state.results} />
-          </section>
-        </div>
-      </div>
-    );
-  }
+    componentDidMount() {
+	console.log("in component did mount")
+
+	let doc = new GoogleSpreadsheet('194rof1NCeXCIGIaPLQu61mj23-YeIFyJA4hQc8q7UQE', null, {gzip: false});
+	let sheet;
+	let names;
+	let rows = [];
+	doc.getInfo(function(err, info) {
+	    if( err ) {
+		console.log(err);
+	    }
+	    console.log('Loaded doc: '+info.title+' by '+info.author.email);
+	    sheet = info.worksheets[0];
+	    console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+' x '+sheet.colCount);
+	    sheet.getRows({offset: 1, limit: 11}, function(err, rs) {
+		if( err ) {
+		    console.log(err);
+		}
+		rows=rs;
+		console.log("example row: " + rows[10].comedyclubfestival);
+		console.log("getRows rows: " + rows);
+	    });
+	    console.log("getInfo rows: " + rows);
+    });
+    console.log("outside rows: " + rows) // this is empty, can I set this.state.results to the actual rows that was returned?
+}
+    
+    render() {
+	console.log("in render")
+	console.log(this.state.results)
+	let hasResults = this.state.results.length > 0
+	if (hasResults) {
+	    return (
+		<div className="content">
+		    <div className="container">
+			<section className="section">
+			    <Results results={this.state.results} />
+			</section>
+		    </div>
+		</div>
+	    );
+	};
+    }
 }
 
 const rootElement = document.getElementById("app");
