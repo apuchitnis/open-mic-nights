dde408f20ccc9f06cbbad0f094572dbad744b37b
Apurva Chitnis <apuchitnis@hotmail.com>
2019-09-22 00:13:40 +0100

    add analytics and bringer

--- app.js
+++ app.js
@@ -10,32 +10,51 @@ class Results extends React.Component {
     render() {
 	return (
 	    <div>
-		<input
-		    type="text"
-		    className="input"
-		    onChange={this.props.handleChange}
-		    placeholder="Search..."
-		/>
-		<table className="table">
-		    <thead>
-			<tr>
-			    <th>name</th>
-			    <th>city</th>
-			    <th>venue</th>
-			    <th>bringer</th>
-			</tr>
-		    </thead>
-		    <tbody>
-			{this.props.results.map(item => (
-			    <tr key={item.comedyclubfestival}>
-				<th>{item.comedyclubfestival}</th>
-				<td>{item.city}</td>
-				<td>{item.venue}</td>
-				<td>{item.bringer}</td>
+		<div class="field">
+		    <div class="control">
+			<input
+			type="text"
+			id="search"
+			className="input"
+			onChange={this.props.handleChange}
+			placeholder="Search..."		
+			/>
+		    </div>
+		</div>
+
+		<div class="field">
+		    <label className="checkbox is-pulled-right">
+			<div class="control">
+			    <input
+				type="checkbox"
+				id="bringer"
+				onChange={this.props.handleChange}
+			    />
+			    bringer night
+			</div>
+		    </label>
+		</div>
+
+		    <table className="table">
+			<thead>
+			    <tr>
+				<th>name</th>
+				<th>city</th>
+				<th>venue</th>
+				<th>bringer</th>
 			    </tr>
-			))}
-		    </tbody>
-		</table>
+			</thead>
+			<tbody>
+			    {this.props.results.map(item => (
+				<tr key={item.comedyclubfestival}>
+				    <th>{item.comedyclubfestival}</th>
+				    <td>{item.city}</td>
+				    <td>{item.venue}</td>
+				    <td>{item.bringer}</td>
+				</tr>
+			    ))}
+			</tbody>
+		    </table>
 	    </div>
 	);
     }
@@ -70,10 +89,6 @@ class App extends React.Component {
 		    return;
 		}
 
-		var keys = Object.keys(response[0])
-		console.log(response[0])
-		console.log(keys)
-
 		this.setState(
 		    {
 			results: response,
@@ -95,26 +110,39 @@ class App extends React.Component {
 		    <section className="section">
 			<Results 
 			    results={this.state.results}
-			    handleChange={(e) => this.handleChange(e)}
+			    handleChange={() => this.handleChange()}
 			/>
 		    </section>
 		</div>
+		<h5 className="title has-text-centered">
+		    Created with ❤ by <a href="https://github.com/apuchitnis">@apuchitnis</a>.
+		</h5>
 	    </div>
 	);
     }
 
-    handleChange(e) {
+    handleChange() {
 	let newResults = this.state.rows;
 
-	if (e.target.value !== "") {
+	var search = document.getElementById('search')
+	if (search.value !== "") {
 	    newResults = newResults.filter(item => {
 		const comedyclubfestival = item.comedyclubfestival.toLowerCase();
 		const city = item.city.toLowerCase();
 		const venue = item.venue.toLowerCase();
-		const filter = e.target.value.toLowerCase();
+		const filter = search.value.toLowerCase();
 		return comedyclubfestival.includes(filter) || city.includes(filter) || venue.includes(filter);
 	    });
 	}
+
+	var bringer = document.getElementById('bringer')
+	newResults = newResults.filter(item => {
+	    if ((item.bringer && bringer.checked) || (!item.bringer && !bringer.checked)) {
+		return true;
+	    }
+	    return false;
+	});
+	
 
 	this.setState({
 	    results: newResults
