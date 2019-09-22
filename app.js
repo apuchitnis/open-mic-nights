8966681be1944e873459fd38f17a853c463947c5
Apurva Chitnis <apuchitnis@hotmail.com>
2019-09-22 12:35:45 +0100

    Add facebook and website link.

--- app.js
+++ app.js
@@ -33,31 +33,32 @@ class Results extends React.Component {
 		    <thead>
 			<tr>
 			    <th>name</th>
+			    <th>frequency</th>
 			    <th>city</th>
 			    <th>venue</th>
-			    <th>bringer</th>
+			    <th>bringer night</th>
 			</tr>
 		    </thead>
 		    <thead>
 			<tr>
 			    <th></th>
 			    <th></th>
-			    <th>
-			    </th>
+			    <th></th>
+			    <th></th>
 			    <th>
 				<div className="dropdown" onClick={this.toggleIsActive}>
 				    <div className="dropdown-trigger">
 					<button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-					    <span id="bringer">select bringer</span>
+					    <span id="bringer">filter bringer night</span>
 					</button>
 				    </div>
 				    <div className="dropdown-menu" id="dropdown-menu" role="menu">
 					<div className="dropdown-content">
 					    <a className="dropdown-item" onClick={(e) => {this.setButtonText(e); this.props.handleChange(e);}}>
-						yes
+						only bringer night
 					    </a>
 					    <a className="dropdown-item" onClick={(e) => {this.setButtonText(e); this.props.handleChange(e);}}>
-						no
+						not bringer night
 					    </a>
 					    <a className="dropdown-item" onClick={(e) => {this.setButtonText(e); this.props.handleChange(e);}}>
 						don't care
@@ -71,7 +72,19 @@ class Results extends React.Component {
 		    <tbody>
 			{this.props.results.map(item => (
 			    <tr key={item.comedyclubfestival}>
-				<th>{item.comedyclubfestival}</th>
+				<th>
+				    <a href={item.facebook}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
+					</svg>
+				    </a>
+				    <span> </span>
+				    {item.website != ""?
+				     <a href={item.website}>
+					 {item.comedyclubfestival}
+				     </a> : item.comedyclubfestival
+				    }
+				</th>
+				<td>{item.frequency}</td>
 				<td>{item.city}</td>
 				<td>{item.venue}</td>
 				<td>{item.bringer}</td>
@@ -161,7 +174,7 @@ class App extends React.Component {
 	
 	var bringer = document.getElementById('bringer').textContent
 	newResults = newResults.filter(item => {
-	    if ((item.bringer && bringer==="yes") || (!item.bringer && bringer==="no") || (bringer==="don't care")) {
+	    if ((item.bringer && bringer==="only bringer night") || (!item.bringer && bringer==="not bringer night") || (bringer==="don't care")) {
 		return true;
 	    }
 	    return false;
